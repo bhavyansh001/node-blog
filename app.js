b07cf868d4fa9e747bd37029fa10e92258c6740b
Bhavyansh <bhavyansh001@gmail.com>
2025-02-17 22:17:23 +0530

    feat: Refactor viewRoutes code in viewBlogController, remove redundant code from app.js

--- app.js
+++ app.js
@@ -17,16 +17,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api/blogs', apiBlogRoutes); // API Routes (JSON)
 app.use('/', viewRoutes);             // View Routes (HTML)
 
-const { pool } = require('./config/database');
-app.get('/', async (req, res) => {
-  try {
-    const result = await pool.query('SELECT * FROM blogs ORDER BY id DESC');
-    res.render('index', { blogs: result.rows });
-  } catch (err) {
-    res.status(500).send('Error loading blog posts');
-  }
-});
-
 initializeDatabase().then(() => {
     app.listen(PORT, () => {
       console.log(`Server is running on http://localhost:${PORT}`);
